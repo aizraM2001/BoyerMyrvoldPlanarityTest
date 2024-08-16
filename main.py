ba985f62a7648ab6df521d8b9a5585d81002791f
aizraM2001 <120380467+aizraM2001@users.noreply.github.com>
2024-08-16 18:32:23 +0200

    main.py

--- main.py
+++ main.py
@@ -1,65 +1,37 @@
-from GraphV2 import *
+import networkx as nx
+import GraphV2 
+from inputV2 import g
+import plot 
 
+#print(g)
 
-""" g = Graph()
-edges = [
-    (1, 2), (1, 3),
-    (2, 4), (2, 5),
-    (3, 6), (3, 7), (3,8),
-    (4, 8), (4, 9),
-    (5, 10), (5, 11),
-    (6, 12), (6, 13),
-    (7, 14), (7, 15), (12, 4)
-] """
+gt = nx.Graph()
+#reale = 1
 
-# Creiamo un grafo con 15 vertici e 17 archi
-g = Graph()
+for i in range(g.DFI-1, 0,-1):
+    #print(i)
 
-# Aggiungiamo gli archi al grafo
-""" edges = [
-    ('A', 'B'), ('A', 'C'),
-    ('B', 'D'), ('B', 'E'),
-    ('C', 'F'), ('C', 'G'), ('C', 'H'),
-    ('D', 'H'), ('D', 'I'),
-    ('E', 'J'), ('E', 'K'),
-    ('F', 'L'), ('F', 'M'),
-    ('G', 'N'), ('G', 'O'), ('L', 'D'),  ('K', 'A') #('O', 'A'),
-]
+    for j in g.DFStree.neighbors(i):
+        if j>i:
+            #print("Arco:", i, j)
+            cb= nx.Graph()
+            cb.add_node(i, DFI = g.getDFI(i), name = g.getName(i))
+            cb.add_node(j, DFI = g.getDFI(j), name = g.getName(j))
+            cb.add_edge(i, j)
+            cb.nodes[j]["reale"] = True
+            cb.nodes[i]["reale"] = i
+            cb.nodes[i]["root"] = True
+            #sd = g.creaStrutturaListe(cb,i,[])
+            nx.set_node_attributes(cb, str(i) + str(j), 'graph_id')
+            gt = nx.disjoint_union(gt, cb)
+            #print(gt)
+            #plot.printGrafo(gt)
 
-g.add_edges_from(edges)
-g.printDFStree()
-g.printComponentiDFS()
-g.printLowpoint()
-g.ordinaLowpoint()  """
+    for e in g.G.nodes[g.getName(i)]["backEdgeList"]:
+        print(f"nodo: {i}, backedge: {e}")
+        v, w = e
+        #print("sd: ", sd)
+        g.walkup(gt, v, w)
+            
 
-"""
-
-g6 = Graph()
-edges = [
-    (1, 2), (1, 3),
-    (2, 4), (2, 5),
-    (3, 6), (3, 7), (3,8),
-    (4, 8), (4, 9),
-    (5, 10), (5, 11),
-    (6, 12), (6, 13),
-    (7, 14), (7, 15), (12, 4), (15,1), (11, 1)
-]
-g6.add_edges_from(edges)
-g6.printDFStree()
-g6.printComponentiDFS()
-g6.printLowpoint() 
-g6.ordinaLowpoint() """
-
-g6 = Graph()
-edges = [
-    (0, 1), (0, 2),
-    (1, 2), (2, 3),
-    (2, 4), (2, 6), (3,4),
-    (3, 5), (4, 5),
-    (4, 6), (5, 6)
-]
-g6.add_edges_from(edges)
-g6.printDFStree()
-g6.printComponentiDFS()
-g6.printLowpoint() 
-g6.ordinaLowpoint() 
+        
